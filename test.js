22f76f65a2568ca3b70b695f7be4a39432aba2a7
a4a881d4 <a4a881d4@163.com>
2014-02-16 23:14:22 +0800

    neo4j

--- test.js
+++ test.js
@@ -1,7 +1,44 @@
 var rpc = require('./lib/coinrpc');
+var db = require('./lib/neo4japi');
 
-rpc.top( function( h ) {
-	rpc.byHeight(h,console.log);
-});
 
+var clean = function( num ) {
+	array = new Array();
+	for (i = 0; i < num; i++) {
+	    array[i] = i;
+	}
+
+	array.forEach( function( h ) {
+		db.forNodeByID( h, function( node ) {
+			console.log( node );
+			db.delNode( node, function(){} )
+		});
+	});
+};
+
+
+
+var createBlock = function( num ) {
+	array = new Array();
+	for (i = 0; i < num; i++) {
+	    array[i] = i;
+	}
+
+	array.forEach( function( h ) {
+		rpc.byHeight( h, function( blk ) {
+			db.saveNode( blk, console.log );
+		});
+	});
+};
+
+//db.cleanNodes('type','block');
+createBlock(2);
+
+
+/*
+	rpc.height( h, function( blk ) {
+		console.log( blk );
+		rpc.block( blk, console.log );
+	});
+*/
 
